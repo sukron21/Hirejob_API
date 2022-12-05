5664e3a5fd6729cf938253c4339eea4603fb7f7e
lol <Rahmatfurqon0@gmail.com>
2022-12-05 08:12:54 +0700

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,11 @@
 
 //declare library
 const pg = require("pg");
-const { DB_DATABASE, DB_LOCALHOST, DB_USERNAME, DB_PASSWORD, DB_PORT } = require('../helper/env')
+const { DB_DATABASE, DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_PORT } = require('../helper/env')
 
 try{
     const db = new pg.Pool({
-        host: DB_LOCALHOST,
+        host: DB_HOSTNAME,
         user: DB_USERNAME,
         password: DB_PASSWORD,
         database: DB_DATABASE,
